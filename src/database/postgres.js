f4535f79658854f94339d74947db5a1540ee417f
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-08-08 11:38:01 -0300

    heroku

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -2,31 +2,16 @@ import pg from 'pg';
 import dotenv from "dotenv"
 dotenv.config()
 
-// console.log('database rodou')
-
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = 'postgres';
-const host = 'localhost';
-const port = 5432;
-const database = 'shortURL';
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
 
-const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
-});
-
-// const query = connection.query('SELECT * FROM users');
-
-// query.then(result => {
-//   console.log(result.rows);
-// });
-
-
+const connection = new Pool(databaseConfig);
 
 export default connection;
